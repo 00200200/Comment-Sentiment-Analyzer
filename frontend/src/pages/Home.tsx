6b326fe9b71d2d4cf4504212e346a7596086efc7
Michał Furgała <83299832+00200200@users.noreply.github.com>
2025-03-11 16:42:50 +0100

    init

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,24 +2,28 @@ import React, { useState } from 'react';
 
 const Home: React.FC = () => {
 	const [url, setUrl] = useState('');
+
 	const handleAnalyze = () => {
-		console.log('Analizuję komentarze dla linku:', url);
+		console.log('Analyzing :', url);
 	};
+
 	return (
-		<div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-			<h1 className='text-4xl font-bold mb-6'>YouTube Comment Analyzer</h1>
+		<div className='flex flex-col items-center justify-center min-h-screen '>
+			<h1 className='text-4xl font-bold mb-6 text-primary'>YouTube Comment Analyzer</h1>
 			<div className='w-full max-w-md mb-4'>
 				<input
 					type='text'
 					placeholder='Paste URL'
 					value={url}
 					onChange={e => setUrl(e.target.value)}
-					className='w-full p-3 rounded border border-gray-300 focus:outline-none focus:border-blue-500'
+					className='w-full p-3 rounded border border-border 
+                     focus:outline-none focus:border-primary'
 				/>
 			</div>
 			<button
 				onClick={handleAnalyze}
-				className='px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors'>
+				className='px-6 py-3 bg-primary text-white rounded 
+                   hover:bg-secondary transition-colors'>
 				Analyze
 			</button>
 		</div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				main: '#f8fafc', 
+				primary: '#3b82f6',
+				secondary: '#1d4ed8',
+				border: '#cbd5e1', 
+			},
+		},
 	},
 	plugins: [],
 };
